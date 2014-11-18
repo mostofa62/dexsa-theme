167b7ed9571c093616119ddfbbe553253448b4cc
mostofa62 <golammostofa31188@gmail.com>
2014-11-18 23:34:47 +0600

    modified recent post widget

--- inc/widgets.php
+++ inc/widgets.php
@@ -24,7 +24,10 @@ $title = apply_filters('widget_title', empty($instance['title']) ? __('Recent Po
 if( empty( $instance['number'] ) || ! $number = absint( $instance['number'] ) )
 $number = 10;
 
-$r = new WP_Query( apply_filters( 'widget_posts_args', array( 'posts_per_page' => $number, 'no_found_rows' => true, 'post_status' => 'publish', 'ignore_sticky_posts' => true ) ) );
+if( empty( $instance['category'] ) || ! $category = absint( $instance['category'] ) )
+$category='';
+
+$r = new WP_Query( apply_filters( 'widget_posts_args', array( 'posts_per_page' => $number, 'no_found_rows' => true, 'post_status' => 'publish', 'ignore_sticky_posts' => true ,'category__in' => $category) ) );
 
 
 if( $r->have_posts() ) :
@@ -51,6 +54,66 @@ endif;
 
 
 }
+
+
+function form($instance) {
+	
+	
+	if( $instance) {
+		$title = esc_attr($instance['title']);
+		$number = esc_attr($instance['number']);
+		$category=esc_attr($instance['category']);
+		
+	} else {
+		$title = '';
+		$number = '';
+		$category='';
+	}
+	?>
+		<p>
+		<label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title', 'dexsa'); ?></label>
+		<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" />
+		</p>
+		<p>
+		<label for="<?php echo $this->get_field_id('number'); ?>"><?php _e('Number of Posts:', 'dexsa'); ?></label>		
+		<select id="<?php echo $this->get_field_id('number'); ?>"  name="<?php echo $this->get_field_name('number'); ?>">
+			<?php for($x=1;$x<=10;$x++): ?>
+			<option <?php echo $x == $number ? 'selected="selected"' : '';?> value="<?php echo $x;?>"><?php echo $x; ?></option>
+			<?php endfor;?>
+		</select>
+		</p>
+        <p>
+		<label for="<?php echo $this->get_field_id('category'); ?>"><?php _e('Select Post Category:', 'dexsa'); ?></label>
+        <?php //wp_dropdown_categories( 'show_count=1&hierarchical=1' );
+		wp_dropdown_categories(
+		array(
+		//'show_option_all'=>' ',
+		'show_option_none'=>'None',
+		'show_count'=>1,
+		'hierarchical'=>1,
+		'id'=>$this->get_field_id('category'),
+		'name'=>$this->get_field_name('category'),
+		'class'=>'widefat',
+		'selected'=>$category		
+		));
+		 ?>
+        </p>
+        		 
+	<?php
+	}
+
+
+function update($new_instance, $old_instance) {
+      $instance = $old_instance;
+	      
+	      $instance['title'] = strip_tags($new_instance['title']);
+	      $instance['number'] = strip_tags($new_instance['number']);
+	      $instance['category'] = strip_tags($new_instance['category']);
+	     return $instance;
+}
+
+
+
 
 
 }
